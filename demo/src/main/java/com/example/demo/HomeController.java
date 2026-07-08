2e40e94df6208eb05f87d4151aaf4efa9a17161e
Anand kumar <154907508+anandkumar-a@users.noreply.github.com>
2026-07-08 12:02:25 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class HomeController {
+    @GetMapping
+    public String home() {
+        return "Spring Boot is Running";
+    }
+}
